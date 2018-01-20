58293b99ddcece32e97b47e6aca9bd6f45181b93
sherah <2359932265@qq.com>
2018-01-20 18:20:29 +0800

    首页首次提交

--- src/components/addAddressComponent/addAddress.vue
+++ src/components/addAddressComponent/addAddress.vue
@@ -31,6 +31,7 @@
 </template>
 <script type="text/javascript">
     import './addAddress.scss'
+    import spinner from "../spinnerComponent/spinner"
     import http from '../../utils/reqAjax'
     import { Field,Button,MessageBox,Popup} from 'mint-ui';
     export default{
@@ -62,7 +63,7 @@
                 }
             },
             save(){
-
+                    
                     this.data.village=$('.site').find('input').val();               
                     //判断是否为有效电话
                      var ph=new RegExp(/^1[34578]\d{9}$/).test(this.data.phone);
@@ -99,14 +100,16 @@
                     return;
                    }
                 } 
-                //填写完整信息后     
+                //填写完整信息后
+                spinner.loadspinner();     
                 var $data=JSON.stringify(this.data);
-                    if(!this.editID){//保存新地址
+                    if(!this.editID){//保存新地址           
                      http.get({url:this.url+'?checkData='+$data}).then(result=>{
                         var row=result.data.data2[0].row;
                         if(row=="0"){
                             http.post({url:this.url,parmas:{data:$data}}).then(res=>{
-                                if(res.data){ 
+                                if(res.data){
+                                    spinner.closeSpinner(); 
                                     this.tip="地址添加成功";
                                     this.success=true;
                                     $('input').val(''); 
@@ -118,6 +121,7 @@
                             })
                         }
                         else{
+                            spinner.closeSpinner();
                             this.tip="收货人信息已存在";
                             this.success=true;
                                     window.setTimeout(()=>{
@@ -130,29 +134,31 @@
                 //保存更新的信息
                 http.post({url:this.url,parmas:{updata:$data,receiveId:this.editID}}).then(res=>{
                     if(res.data){
+                        spinner.closeSpinner();
                         this.tip="信息更新成功";
                         this.success=true; 
                          window.setTimeout(()=>{
                             this.success=false;     
                         },1000); 
                     }else{
+                        spinner.closeSpinner();
                         this.tip="更改信息失败";
                         this.success=true;
                              window.setTimeout(()=>{
                                 this.success=false;
+                                 this.$router.push({name:'getAddress'});
                             },1000);
                         
                         }
-                })
-              
-                
-                
+                }) 
                 
             },
             del(){
                MessageBox.confirm('确定删除该地址信息?').then(action => {
+                   spinner.loadspinner();
                     http.post({url:this.url,parmas:{delId:this.editID}}).then(res=>{
                     if(res.data){
+                        spinner.closeSpinner();
                         this.tip="信息删除成功";
                         this.success=true; 
                          window.setTimeout(()=>{
@@ -162,6 +168,7 @@
                         
                     }
                     else{
+                        spinner.closeSpinner();
                         this.tip="更改信息失败";
                         this.success=true;
                         window.setTimeout(()=>{
@@ -175,10 +182,15 @@
         },
         mounted(){
             if(this.editID){
+                 spinner.loadspinner();
                 http.get({url:this.url+"?id="+this.editID}).then(res=>{
-                this.data={linkMan:res.data[0].linkMan,gender:res.data[0].gender,phone:res.data[0].phone,village:'',doorplate:res.data[0].doorplate,type:res.data[0].type};
-                $('.site').find('input').val(res.data[0].village);
+                if(res.data){
+                     spinner.closeSpinner();
+                     this.data={linkMan:res.data[0].linkMan,gender:res.data[0].gender,phone:res.data[0].phone,village:'',doorplate:res.data[0].doorplate,type:res.data[0].type};
+                    $('.site').find('input').val(res.data[0].village);
 
+               
+                }
                 })
                 
             }
