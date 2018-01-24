9d91eacdfabe75c920659502af5635acf8646bfc
liangme <869764343@qq.com>
2018-01-24 11:03:39 +0800

    首页公共部分

--- back/src/router/index.js
+++ back/src/router/index.js
@@ -2,16 +2,21 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import LoginComponent from '../components/login/login.vue'
+import root from '../components/RootComponent/RootComponent.vue'
 
 Vue.use(VueRouter);
 
 var router = new VueRouter({
     routes:[
-    {
-        path: '/' ||'/login',
-        name: '' || 'login',
-        component: LoginComponent
-    }
+        {
+            path: '/',
+            name: '',
+            component: LoginComponent
+        },{
+            path: '/root',
+            name: 'root',
+            component: root
+        }
     ]
  })
 export default router;
