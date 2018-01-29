0cf41033730756a8353ed6e4e74f6a2538e4a8bf
liangme <869764343@qq.com>
2018-01-29 11:45:16 +0800

    整合

--- front/api/home.php
+++ front/api/home.php
@@ -2,48 +2,19 @@
     header('Access-Control-Allow-Origin:*');
     header("Content-Type: text/html;charset=utf-8"); 
     include "DBHelper.php";
-    $allType=isset($_GET['allType'])?$_GET['allType']:'';
+    $type=isset($_GET['type'])?$_GET['type']:'';
     $uid=isset($_GET['uid'])?$_GET['uid']:'';
-    $sql1='';
     if($uid){
-        $sql="select * from car c,product p where c.userId=$uid and c.goodId=p.goodId;";
-        $result =query_oop($sql);
+        $sql="select * from car where userId=$uid";
     }
-   else if($allType){
-       $sql="";
-       $typeArr=explode(',',$allType);
-       foreach($typeArr as $val){
-            $sql.="select * from product,productimg where  product.goodId=productimg.goodId and product.classifySmallId in (select classifySmallId from classifysmall  where classifyBigId=(select categoryId from category where categoryName='$val') );";
-         }
-        $result =multi_query_oop($sql);
-        
+   else if($type){
+        $sql="select * from product,productimg where  product.goodId=productimg.goodId and product.classifySmallId in (select classifySmallId from classifysmall  where classifyBigId=(select categoryId from category where categoryName='$type') )";  
+        // var_dump($sql);
     }
     else{
-        $sql="select * from category";
-        $result =query_oop($sql); 
+      $sql="select * from category";
+
     }
-    // // arr->obj
-    // function array2object($array) {
-    // if (is_array($array)) {
-    //     $obj = new StdClass();
-    //     foreach ($array as $key => $val){
-    //     $obj->$key = $val;
-    //     }
-    // }
-    // else { $obj = $array; }
-    // return $obj;
-    // }
-    // // obj->arr
-    // function object2array($object) {
-    // if (is_object($object)) {
-    //     foreach ($object as $key => $value) {
-    //     $array[$key] = $value;
-    //     }
-    // }
-    // else {
-    //     $array = $object;
-    // }
-    // return $array;
-    // }  
-      echo json_encode($result, JSON_UNESCAPED_UNICODE);     
+     $result =query_oop($sql);
+     echo json_encode($result, JSON_UNESCAPED_UNICODE); 
 ?>
